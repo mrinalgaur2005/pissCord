1aa51e42f7c56dee3f51645c17b9e86fc87daeed
Mrinal Gaur <mrinalgaur2005@gmail.com>
2024-07-27 20:15:15 +0530

    done the setup of clerk auth

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,14 @@
+const authLayout = ({
+    children,
+    }: Readonly<{
+    children: React.ReactNode;
+    }>) => {
+    return ( 
+        <div className="h-full flex items-center justify-center">
+            {children}
+        </div>
+        
+     );
+}
+ 
+export default authLayout;
--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <p className="text-3xl font-bold text-red-600">
-      HIIIII
+      <UserButton afterSwitchSessionUrl="/"/>
     </p>
   );
 }
