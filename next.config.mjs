79e379faf06cd0e905b27946d24793ee4fda18cb
Mrinal Gaur <mrinalgaur2005@gmail.com>
2024-07-30 00:39:05 +0530

    made the upload image and create server page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ["uploadthing.com", "utfs.io"],
+    },
+  };
+  
+  export default nextConfig;
